dc2a2cbaa04f802171759063f9601dbf88451c1b
BriannaKing2073 <Brianna.KIng.2073@gmail.com>
2025-04-24 11:52:38 -0400

    Added Loaded option for the Sandwich Shop

--- BasicCalulator/src/main/java/com/pluralsight/BasicCalulator.java
+++ BasicCalulator/src/main/java/com/pluralsight/BasicCalulator.java
@@ -8,6 +8,7 @@ public class BasicCalulator {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner (System.in);
+
        //need to print out statement to trigger each prompt
         System.out.println("Enter the first number");
         int num1 = scanner.nextInt();
@@ -18,8 +19,12 @@ public class BasicCalulator {
         int num2 = scanner.nextInt();
 
         System.out.println("Possible Calulations:" +
-                "\n(A)dd \n(S)ubtract \n(M)ultiply \n(D)ivide");
-        int calulations = scanner.nextInt();
+                "\n(A)dd \n(S)ubtract \n(M)ultiply \n(D)ivide\n"+"Please Select an option");
+        String calulations = scanner.nextLine();
+
+        int operation = (num1+ calulations + num2);
+        System.out.printf("%d %s %d = %d", num1, calulations, num2,operation);
+
 
     }
 }
--- SandwichShop/src/main/java/SandwichShop/SandwichShop.java
+++ SandwichShop/src/main/java/SandwichShop/SandwichShop.java
@@ -6,7 +6,7 @@ public class SandwichShop {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
     //Ask Sandwich Size
-        System.out.println("What sized Sandwich would you like today?\n Regular($5.45) or Large($8.95)");
+        System.out.println("What sized Sandwich would you like today?\n Regular($5.45) or Large($8.95) \n(Regular/Large):");
         String sandwichSize = scanner.nextLine();
      //Response to Sandwich size
         double price = 0.0;
@@ -18,20 +18,51 @@ public class SandwichShop {
             System.out.println("Sorry we don't have that size.");
         }
         System.out.println(sandwichSize +" $" + price);
+
+        //Loaded options
+        System.out.println("Would you like a loaded Sandwich?\n Yes or No");
+        String loadedSandwich = scanner.nextLine();
+
+        if (sandwichSize.equalsIgnoreCase("Regular") && loadedSandwich.equalsIgnoreCase("Yes")){
+
+            price = 6.45;
+
+        }
+
+       else if (sandwichSize.equalsIgnoreCase("Large") && loadedSandwich.equalsIgnoreCase("Yes")){
+
+           price = 10.70;
+
+       }
+
+        else {
+            sandwichSize = sandwichSize;
+            price = price;
+        }
+       System.out.println("Your Order:\n"+ sandwichSize + "\nLoaded: " + loadedSandwich + "\nSubtotal: $" + price);
         //Ask for age
         System.out.println("Enter your age to see if you are eligible for a discount");
-        String age = scanner.nextLine();
+        int age = scanner.nextInt();
+        //catches Enter
+        scanner.nextLine();
         
         //discount conditions
         double total = price;
 
-        if (age <= 17);
-        {total = price - 0.10;}
-        else if (age >= 65 );
-        {total = price - 0.20;}
-        else {total = price;}
+        if (age <= 17) {
+            total = price - 0.10;
+            System.out.println("Your Order:\n"+ sandwichSize + "\nLoaded: " + loadedSandwich + "\nTotal: $" + total + "\n 10% Discount");
+        }
+        else if (age >= 65 ) {
+            total = price - 0.20;
+            System.out.println("Your Order:\n"+ sandwichSize + "\nLoaded: " + loadedSandwich + "\nTotal: $" + total + "\n20% Discount");
+        }
+        else {
+            total = price;
+            System.out.println("Your Order:\n"+ sandwichSize + "\n Loaded: " + loadedSandwich + "\n Total: $" + total);
+        }
 
-        System.out.println(sandwichSize + " $"+ total);
+
 
         }
     }
